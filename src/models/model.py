a2a4398482f942727eed38577110adc3fa33104f
osta0005 <olinestaerke@hotmail.com>
2021-06-10 12:29:57 +0200

    Training working (I think)

--- src/models/model.py
+++ src/models/model.py
@@ -3,18 +3,20 @@ import random
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, AdamW
 import numpy as np
-# Use a GPU if you have one available (Runtime -> Change runtime type -> GPU)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Set seeds for reproducibility
-random.seed(26)
-np.random.seed(26)
-torch.manual_seed(26)
+def myModel():
+    # Use a GPU if you have one available (Runtime -> Change runtime type -> GPU)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-tokenizer = AutoTokenizer.from_pretrained("roberta-base") 
+    # Set seeds for reproducibility
+    random.seed(26)
+    np.random.seed(26)
+    torch.manual_seed(26)
 
-model = AutoModelForSequenceClassification.from_pretrained("roberta-base")
-model.to(device) # Send the model to the GPU if we have one
+    tokenizer = AutoTokenizer.from_pretrained("roberta-base") 
 
-learning_rate = 1e-5
-optimizer = AdamW(model.parameters(), lr=learning_rate, eps=1e-8)
+    model = AutoModelForSequenceClassification.from_pretrained("roberta-base")
+    model.to(device) # Send the model to the GPU if we have one
+
+
+    return model
