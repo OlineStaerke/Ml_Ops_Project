fed45c640a967f47524f56f0cef49993166c7ba4
adamzenith <matthiasadamsen@gmail.com>
2021-06-10 13:53:40 +0200

    new stuff

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -17,15 +17,11 @@ def encode_data(dataloader, max_length):
     """Encode the question/passage pairs into features than can be fed to the model."""
     input_ids = []
     attention_masks = []
-    answers = []
-   
-    
+
     for values in dataloader:
-        answer = values['answer']
         question = values['question']
         passage = values['passage']
         
-        answers.append(answer)
 
         encoded_data = tokenizer.encode_plus(question, passage, max_length=max_length, pad_to_max_length=True, truncation_strategy="longest_first")
         encoded_pair = encoded_data["input_ids"]
@@ -35,7 +31,7 @@ def encode_data(dataloader, max_length):
         attention_masks.append(attention_mask)
         
 
-    return np.array(input_ids), np.array(attention_masks), answers
+    return np.array(input_ids), np.array(attention_masks)
 
 
 
@@ -44,27 +40,29 @@ def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    logger = logging.getLogger(__name__)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(dir_path)
 
+    logger.info('> Loading Dataset')
     train_dataset = load_dataset("boolq",split="train")
     validation_dataset = load_dataset("boolq",split="validation")
     
   
-    #Save not encoded data
+    #Save raw data
     #torch.save(train_dataset,"../../data/raw/train.pt")
     #torch.save(validation_dataset,"../../data/raw/test.pt")
 
+    logger.info('> Encoding raw data')
     max_seq_length = 256
-    input_ids_train, attention_masks_train, answers_train = encode_data(train_dataset,max_seq_length)
-    input_ids_dev, attention_masks_dev, answers_dev = encode_data(validation_dataset,max_seq_length)
+    input_ids_train, attention_masks_train = encode_data(train_dataset,max_seq_length)
+    answers_train = np.array([int(a) for a in train_dataset['answer']])
+    input_ids_dev, attention_masks_dev = encode_data(validation_dataset,max_seq_length)
+    answers_dev = np.array([int(a) for a in validation_dataset['answer']])
 
     train_features = (input_ids_train, attention_masks_train, answers_train)
     dev_features = (input_ids_dev, attention_masks_dev, answers_dev)
     
-
-    #Creating the dataloader
-
     # Building Dataloaders
     batch_size = 32
 
@@ -84,18 +82,10 @@ def main():
     torch.save(train_dataloader,"../../data/processed/train.pt")
     torch.save(dev_dataloader,"../../data/processed/test.pt")
 
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
 
     main()
