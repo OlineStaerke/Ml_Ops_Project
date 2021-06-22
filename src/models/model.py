4e917eb1a4986a34776ed6649d1d591d0cef75d9
osta0005 <olinestaerke@hotmail.com>
2021-06-22 10:14:00 +0200

    Added OPTUNA - still testing it

--- src/models/model.py
+++ src/models/model.py
@@ -13,7 +13,8 @@ class myModel():
                     epochs = 5,
                     lr = 1e-5,
                     grad_acc_steps = 1,
-                    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+                    device = torch.device("cuda" if torch.cuda.is_available() else "cpu"), 
+                    optimizer = AdamW
                 ):
         # Set seeds for reproducibility
         random.seed(26)
@@ -25,48 +26,74 @@ class myModel():
         self.lr = lr
         self.grad_acc_steps = grad_acc_steps
         self.model = AutoModelForSequenceClassification.from_pretrained("roberta-base")
-        self.optimizer = AdamW(self.model.parameters(), lr=self.lr, eps=1e-8)
+        self.optimizer = optimizer(self.model.parameters(), lr=self.lr, eps=1e-8)
         self.device = device
         self.model.to(self.device) # Send the model to the GPU if we have one
   
 
-    def train(self, train_dataloader):
+    def train(self, train_dataloader, val_dataloader):
 
         train_loss_values = []
         dev_acc_values = []
+        
 
         for _ in tqdm(range(self.epochs), desc="Epoch"):
 
-            # Training
-            epoch_train_loss = 0 # Cumulative loss
-            self.model.train()
-            self.model.zero_grad()
+        #     # Training
+        #     epoch_train_loss = 0 # Cumulative loss
+        #     self.model.train()
+        #     self.model.zero_grad()
 
-            # tqdm is a Python library that allows you to output a smart progress bar by wrapping around any iterable
-            # leave ensures one continuous progress bar across all loops
-            for step, batch in tqdm(enumerate(train_dataloader),leave=False,total=len(train_dataloader)):
+        #     # tqdm is a Python library that allows you to output a smart progress bar by wrapping around any iterable
+        #     # leave ensures one continuous progress bar across all loops
+        #     for step, batch in tqdm(enumerate(train_dataloader),leave=False,total=len(train_dataloader)):
 
+        #         input_ids = batch[0].to(self.device)
+        #         attention_masks = batch[1].to(self.device)
+        #         labels = batch[2].to(self.device)     
+
+        #         outputs = self.model(input_ids, token_type_ids=None, attention_mask=attention_masks, labels=labels)
+
+        #         loss = outputs[0]
+        #         loss = loss / self.grad_acc_steps
+        #         epoch_train_loss += loss.item()
+
+        #         loss.backward()
+        #         #wandb.log({'outputs:': outputs})
+                
+        #         if (step+1) % self.grad_acc_steps == 0: # Gradient accumulation is over
+        #             torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0) # Clipping gradients
+        #             self.optimizer.step()
+        #             self.model.zero_grad()
+                
+
+        #     epoch_train_loss = epoch_train_loss / len(train_dataloader)          
+        #     train_loss_values.append(epoch_train_loss)
+
+            # Evaluation
+            epoch_dev_accuracy = 0 # Cumulative accuracy
+            self.model.eval()
+
+            for batch in val_dataloader:
+                
                 input_ids = batch[0].to(self.device)
                 attention_masks = batch[1].to(self.device)
-                labels = batch[2].to(self.device)     
-
-                outputs = self.model(input_ids, token_type_ids=None, attention_mask=attention_masks, labels=labels)
-
-                loss = outputs[0]
-                loss = loss / self.grad_acc_steps
-                epoch_train_loss += loss.item()
-
-                loss.backward()
-                wandb.log({'outputs:': outputs})
+                labels = batch[2]
+                            
+                with torch.no_grad():        
+                    outputs = self.model(input_ids, token_type_ids=None, attention_mask=attention_masks)
+                                
+                logits = outputs[0]
+                logits = logits.detach().cpu().numpy()
                 
-                if (step+1) % self.grad_acc_steps == 0: # Gradient accumulation is over
-                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0) # Clipping gradients
-                    self.optimizer.step()
-                    self.model.zero_grad()
+                predictions = np.argmax(logits, axis=1).flatten()
+                labels = labels.numpy().flatten()
                 
+                epoch_dev_accuracy += np.sum(predictions == labels) / len(labels)
 
-            epoch_train_loss = epoch_train_loss / len(train_dataloader)          
-            train_loss_values.append(epoch_train_loss)
-        return train_loss_values
+            epoch_dev_accuracy = epoch_dev_accuracy / len(val_dataloader)
+            dev_acc_values.append(epoch_dev_accuracy)
+
+        return train_loss_values, dev_acc_values
 
         
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -11,16 +11,51 @@ from IPython import embed
 import hydra
 from omegaconf import DictConfig
 import logging
+import optuna
+###############
+#DATA LOADING##
+###############
+def load_data():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(dir_path)
+
+    #Import data
+    print("Current directory:") #Current dorectory changes to log files when using hydra
+    print(os.getcwd())
+    
+    train_dataloader = torch.load("../../../../../data/processed/train.pt")
+    test_dataloader = torch.load("../../../../../data/processed/test.pt")
+
+    return train_dataloader,test_dataloader
+
+########
+#OPTUNA#
+########
+def my_model_optuna(trial):
+    #Get paramters from OPTUNA
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    learning_rate = trial.suggest_discrete_uniform('lr',0.0001,0.1,1)
+    epochs = int(trial.suggest_discrete_uniform('epochs',10,100,1))
+    optimizer =  trial.suggest_categorical('optimizer',[torch.optim.SGD, torch.optim.RMSprop, torch.optim.AdamW])
+    grad_acc_steps = 1
+    model = myModel(epochs, learning_rate, grad_acc_steps, device, optimizer)
+
+    return model
+    
+def objective(trial):
+    model = my_model_optuna(trial)
+    train_dataloader, val_dataloader = load_data() #load data
+    return(train_model(model, train_dataloader, val_dataloader)) #train model, returns val accuracy
+
 
 #################
 #HYPERPARAMETERS#
 #################
 @hydra.main(config_path="../../", config_name = "config.yaml")
-def my_app(cfg: DictConfig) -> None:
+def my_model_hydra(cfg: DictConfig) -> None:
     #Log in hydra the params
     log = logging.getLogger(__name__)
     log.info(cfg)
-    
 
     #wandb.init(project="ml_ops_squad")
     # Use a GPU if you have one available (Runtime -> Change runtime type -> GPU)
@@ -31,22 +66,13 @@ def my_app(cfg: DictConfig) -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     grad_acc_steps = cfg.grad_acc_steps
 
-    model = myModel(epochs, learning_rate, grad_acc_steps, device)
+    model = myModel(epochs, learning_rate, grad_acc_steps, device) #init model
+    train_dataloader, val_dataloader = load_data() #load data
+    train_model(model, train_dataloader, val_dataloader) #train model
 
-    ###############
-    #DATA LOADING##
-    ###############
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
-
-    #Import data
-    print("Current directory:") #Current dorectory changes to log files when using hydra
-    print(os.getcwd())
+def train_model(model, train_dataloader, val_dataloader):
     
-    train_dataloader = torch.load("../../../../../data/processed/train.pt")
-    test_set = torch.load("../../../../../data/processed/test.pt")
-
     ##################
     #WEIGHTS & BIASES#
     ##################
@@ -57,13 +83,27 @@ def my_app(cfg: DictConfig) -> None:
     #TRAINING#
     ##########
 
-    train_loss = model.train(train_dataloader)
+    train_loss, val_acc = model.train(train_dataloader, val_dataloader )
+
     ########
     #SAVING#
     ########
 
     torch.save(model.model, "../../models/model.pth")
 
+    return val_acc[-1]
+
+
 
 if __name__ == "__main__":
-    my_app()
+    OPTUNA=False
+    if not OPTUNA:
+        model = my_model_hydra()
+    
+    else:
+        study = optuna.create_study(direction="maximize",pruner=optuna.pruners.MedianPruner(
+        n_startup_trials=5, n_warmup_steps=30, interval_steps=10))
+        study.optimize(objective, n_trials=100) 
+
+        
+
