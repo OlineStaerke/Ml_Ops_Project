78a76bdf0ddbb42e83120dd7326aa85df1b894c3
adamzenith <matthiasadamsen@gmail.com>
2021-06-22 15:00:05 +0200

    submitjob

--- submitjob.sh
+++ submitjob.sh
@@ -3,7 +3,7 @@
 ### –- specify queue --
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J alBERT-model
+#BSUB -J alBERT-model_first_trial
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 1
 ### -- Select the resources: 1 gpu in exclusive process mode --
