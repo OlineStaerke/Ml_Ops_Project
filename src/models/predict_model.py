4ee40b9045919de720ea78944fbb047da4db6e28
adamzenith <matthiasadamsen@gmail.com>
2021-06-22 15:07:21 +0200

    tested on trained model

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,35 +1,32 @@
 import torch, os
 from IPython import embed
+import numpy as np
 
-def predict():
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
+print(os.getcwd())
 
-  dir_path = os.path.dirname(os.path.realpath(__file__))
-  os.chdir(dir_path)
-  print(os.getcwd())
+test_dir = "../../data/processed/test.pt"
+test_dataloader = torch.load(test_dir)
+sample = iter(test_dataloader).next()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = torch.load("../../models/model.pth")
 
-  test_dir = "../../data/processed/test.pt"
-  test_dataloader = torch.load(test_dir)
-  sample = iter(test_dataloader).next()
-  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-  model = torch.load("../../models/model.pth")
-  
-  # Evaluation
-  model.eval()
-      
-  input_ids = sample[0].to(device)
-  attention_masks = sample[1].to(device)
-  labels = sample[2]
+# Evaluation
+model.eval()
+    
+input_ids = sample[0].to(device)
+attention_masks = sample[1].to(device)
+labels = sample[2]
 
-  with torch.no_grad():        
-      outputs = model(input_ids, token_type_ids=None, attention_mask=attention_masks)
+with torch.no_grad():        
+    outputs = model(input_ids, token_type_ids=None, attention_mask=attention_masks)
 
-  logits = outputs[0]
-  logits = logits.detach().cpu().numpy()
+logits = outputs[0]
+logits = logits.detach().cpu().numpy()
 
-  predictions = np.argmax(logits, axis=1).flatten()
-  labels = labels.numpy().flatten()
+predictions = np.argmax(logits, axis=1).flatten()
+#labels = labels.numpy().flatten()
+#print(predictions, attention_masks,labels)
+print(predictions)
 
-  return labels
-
-predict()
-        
