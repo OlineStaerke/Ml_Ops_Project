a327f48360109c590e89487d09e3abad6f280f6f
Helena Hansen <helena8hansen@yahoo.fr>
2021-06-22 14:43:48 +0200

    Natasha edit

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,23 +1,31 @@
- # Evaluation
-  epoch_dev_accuracy = 0 # Cumulative accuracy
+import torch
+from IPython import embed
+
+def predict():
+  
+  test_dir = "../../data/processed/test.pt"
+  test_dataloader = torch.load(test_dir)
+  embed()
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  model = torch.load(model.model, "../../models/model.pth")
+  
+  # Evaluation
   model.eval()
+      
+  input_ids = sample[0].to(device)
+  attention_masks = sample[1].to(device)
+  labels = sample[2]
 
-  for batch in dev_dataloader:
-    
-    input_ids = batch[0].to(device)
-    attention_masks = batch[1].to(device)
-    labels = batch[2]
-                
-    with torch.no_grad():        
-        outputs = model(input_ids, token_type_ids=None, attention_mask=attention_masks)
-                    
-    logits = outputs[0]
-    logits = logits.detach().cpu().numpy()
-    
-    predictions = np.argmax(logits, axis=1).flatten()
-    labels = labels.numpy().flatten()
-    
-    epoch_dev_accuracy += np.sum(predictions == labels) / len(labels)
+  with torch.no_grad():        
+      outputs = model(input_ids, token_type_ids=None, attention_mask=attention_masks)
 
-  epoch_dev_accuracy = epoch_dev_accuracy / len(dev_dataloader)
-  dev_acc_values.append(epoch_dev_accuracy)
+  logits = outputs[0]
+  logits = logits.detach().cpu().numpy()
+
+  predictions = np.argmax(logits, axis=1).flatten()
+  labels = labels.numpy().flatten()
+
+  return labels
+
+predict()
+        
