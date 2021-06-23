fbdeae1e08878abe78d93b1583d40bc150d02ad2
Simonwol110 <swl@silvi.ai>
2021-06-23 10:03:22 +0200

    Changed unit testing to check from variable change to non-zero loss, added a few parameters in the model training

--- tests/test_model_train.py
+++ tests/test_model_train.py
@@ -1,9 +1,13 @@
+from random import shuffle
 import pytest
 import torch
 from torch import nn, optim
+from torch.utils.data import DataLoader, random_split
 import os
 from transformers import AdamW
 from src.models.model import myModel
+import numpy as np
+from IPython import embed
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
@@ -18,67 +22,30 @@ def setup():
     # Import model and optimiser
     model = myModel()
     optimizer = AdamW(model.model.parameters(), lr=1e-5, eps=1e-8)
-
+    
     # Import data
-    train_set = torch.load("../data/processed/train.pt")
-    test_set = torch.load("../data/processed/test.pt")
-
-    # Setting up iterator
-    train_iter = iter(train_set)
-    test_iter = iter(test_set)
-    next_train_iter = train_iter.next()
-    batch = [next_train_iter[0], next_train_iter[1], next_train_iter[2]]
-    return model, optimizer, batch
+    train_dataset = torch.load("../data/raw/train.pt")
+    sample_ds = random_split(train_dataset, [30, len(train_dataset) - 30])[0]
+    train_dataloader = DataLoader(sample_ds, batch_size=5, drop_last=True)
+    # train_dataloader = DataLoader(train_set[10], sampler = train_sampler, batch_size=10)
+    return model, optimizer, train_dataloader
 
 
-def _train_step(model, optim, batch):
+def _train_step(model, optim, train_dataloader):
     
     # Put model in training mode
     model.epochs = 1
-    model.model.train()
 
     # Run one forward + backward step 
-    optim.zero_grad()
-    inputs, attention_masks, labels = batch[0], batch[1], batch[2]
+    loss = model.train(train_dataloader, with_wandb=False)
+    return loss
 
-    output = model.model(inputs, token_type_ids=None, attention_mask=attention_masks, labels=labels)
-    loss = output[0]
-    loss.backward()
-    optim.step()
 
-@pytest.fixture
-def _forward_step(model, batch):
+def test_non_zero_loss(setup):
 
-    # Put model in eval mode
-    model.eval()
-
-    with torch.no_grad():
-        # inputs and targets
-        inputs = batch[0]
-        # move data to DEVICE
-        inputs = inputs
-        # forward
-        return model(inputs)
-
-def test_var_change(setup):
-
-    model, optimizer, batch = setup
+    model, optimizer, train_dataloader = setup
     
-    # Take a copy
-    initial_params = [ np for np in model.model.named_parameters() if np[1].requires_grad ]
     # Run a training step
-    _train_step(model, optimizer, batch)
-
-    params = [ np for np in model.model.named_parameters() if np[1].requires_grad ]
-
-    for (_, p0), (name, p1) in zip(initial_params, params):
-        try:
-            assert not torch.equal(p0, p1)
-        except AssertionError:
-            raise VariablesChangeException(
-                "{var_name} {msg}".format(
-                    var_name=name,
-                    msg = 'did not change'
-                )
-            )
+    loss = _train_step(model, optimizer, train_dataloader)
+    assert not (0 in loss)
         
