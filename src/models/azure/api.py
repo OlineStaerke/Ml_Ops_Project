d2beee6862374ff6fd6661767960030636b0df44
osta0005 <olinestaerke@hotmail.com>
2021-06-23 10:30:13 +0200

    added predict

--- src/models/azure/api.py
+++ src/models/azure/api.py
@@ -2,6 +2,30 @@ import json
 from azureml.core import Model
 import joblib
 
+def predict(model, input_ids,attention_masks):
+    # Evaluation
+    model.eval()
+
+    with torch.no_grad():        
+        outputs = model(input_ids, token_type_ids=None, attention_mask=attention_masks)
+
+    print(outputs)
+    
+    # logits = outputs[0]
+    # logits = logits.detach().cpu().numpy()
+
+    # predictions = np.argmax(logits, axis=1).flatten()
+    # #labels = labels.numpy().flatten()
+    # #print(predictions, attention_masks,labels)
+        
+    # for num, element in enumerate(sample[0]):
+    #     element = element.numpy()
+    #     idx = ' '.join([str(elem) for elem in element])
+    #     q, p = lookup[idx]
+    #     pred = predictions[num]
+    #     label = labels[num]
+    #     ic(p, q, pred,label)
+
 
 def init():
     global model
